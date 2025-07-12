f4587086044324b00c3ddba3e4972bff21b26025
xCherryNeirax <164685946+xCherryNeirax@users.noreply.github.com>
2025-07-12 17:50:10 +0200

    UI Separada

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource sfxSource;
+
+    public AudioClip musicMain;
+    public AudioClip musicInGame;
+    public AudioClip musicGameOver;
+    public AudioClip musicGameWinned;
+    public AudioClip Static;
+    public AudioClip tvButtons;
+    public AudioClip openDrawer;
+    public AudioClip closeDrawer;
+    public AudioClip papersounds;
+    public AudioClip acuse;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
